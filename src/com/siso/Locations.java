cc7466e6ff6ac8dd970c3d343073042d79ffd790
Jake (Umut) Siso <umut.sisoo@gmail.com>
2017-09-03 23:45:15 -0500

    Commit

--- src/com/siso/Locations.java
+++ src/com/siso/Locations.java
@@ -49,12 +49,6 @@ public class Locations implements Map<Integer, Location> {
             scanner=new Scanner(new BufferedReader(new FileReader("directions.txt")));
             scanner.useDelimiter(",");
             while(scanner.hasNextLine()){
-//                int loc = scanner.nextInt();
-//                scanner.skip(scanner.delimiter());
-//                String direction = scanner.next();
-//                scanner.skip(scanner.delimiter());
-//                String dest = scanner.nextLine();
-//                int destination = Integer.parseInt(dest);
                 String input = scanner.nextLine();
                 String[] data = input.split(",");
                 int loc = Integer.parseInt(data[0]);
